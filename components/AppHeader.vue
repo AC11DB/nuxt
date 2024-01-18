dced22536d2f6ece7d528377bdfd20074dad665f
AC11DB <cadizangelicamael@gmail.com>
2024-01-18 09:15:10 -0700

    hello

---
+++ components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div> 
+      <NuxtLink to="/">Logo: User name</NuxtLink>
+      </div>
+    <nav>
+      <Nuxt Link to="signup">Sign Up Page</NuxtLink
+    </nav>
+  </header>
+</template>
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  padding: 2rem;
+  color: white;
+  background-color: black;
+}
+a{
+  color
+}
+</style>
