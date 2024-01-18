1606bc9336478eb2337b31693d345f41f7433779
AC11DB <cadizangelicamael@gmail.com>
2024-01-18 14:35:53 -0700

    yo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  //add the module to nuxt
+  // add the module to nuxt
   modules: ["@nuxtjs/supabase"],
-  //turn off required login
+  // turn off required login
   supabase: {
     redirect: false,
   },
 });
-
