298b693e82f2dda8477a656ef2e7f97e506e4e7a
AC11DB <cadizangelicamael@gmail.com>
2024-01-18 14:22:38 -0700

    yo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,3 +8,4 @@ export default defineNuxtConfig({
     redirect: false,
   },
 });
+
---
+++ pages/employees.vue
@@ -0,0 +1,15 @@
+<script setup>
+//fetch the data from a server rout
+const fetchEmployees - async () +> {
+  const ( employees ) = await sfetch("/api/employees");
+};
+const employees = fetchEmployees();
+console.log(employees);
+</script>
+
+<template>
+  <main>
+    <h1>Employees</h1>
+    <!-- render the fethced data-->>
+  </main>
+</template>
