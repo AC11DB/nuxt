be956f3e87137807a06a4ab3a18eb736ae9786d4
AC11DB <cadizangelicamael@gmail.com>
2024-01-18 11:47:03 -0700

    yo

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,24 @@
 <script setup>
-const { data: hello } = await useFetch("/api/hello");
-const { data } = await useFetch("/api/disney");
-const characters = data;
-const clientSideExample = "Updated Blah";
+// Fetch data from the dogs API endpoint
+const { data } = await useFetch("/api/dogs");
+// clean up the data
+const { _value: dataValues } = data;
+const { message: breeds } = dataValues;
 </script>
-
 <template>
   <main>
     <header>
-      <h1>Hello CPRG</h1>
+      <h1>Dog Breeds</h1>
     </header>
-
-    <section>
-      <h2>Hello Data Endpoint Example</h2>
-      <p>{{ hello.hello }}</p>
-      {{ clientSideExample }}
-    </section>
-    <section>
-      <h2>Disney Character Fetch</h2>
+    <div>
+      <pre>{{ breeds }}</pre>
       <ul>
-        <li v-for="character in characters.data" :key="character.id">
-          <h3>{{ character.name }}</h3>
-          <img :src="character.imageUrl" />
+        <!-- Loop through the data to show all dog breeds -->
+        <li v-for="breed in breeds" :key="breed.id">
+          <!-- TODO: Extract breed name from key in k/v pairs -->
+          {{ breed }}
         </li>
       </ul>
-    </section>
+    </div>
   </main>
 </template>
